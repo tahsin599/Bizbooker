3423958577134b43f984b1dd04cc56ea76ab5337
Tahsin <ktahsin280@gmail.com>
2025-05-25 18:38:13 +0600

    ORM Mapping done

--- Backend/src/main/java/com/tahsin/backend/Configuration/SecurityConfig.java
+++ Backend/src/main/java/com/tahsin/backend/Configuration/SecurityConfig.java
@@ -1,8 +1,27 @@
 package com.tahsin.backend.Configuration;
 
+
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .csrf(csrf -> csrf.disable()) // Disable for testing
+        .authorizeHttpRequests(auth -> auth
+            .anyRequest().authenticated()
+        )
+        .httpBasic(Customizer.withDefaults());
+    
+    return http.build();
+}
 
 }
