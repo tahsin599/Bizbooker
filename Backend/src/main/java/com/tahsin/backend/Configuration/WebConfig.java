c5dd8eb650b192c5a3a582ccc3bbf0a617ba8366
tahsin599 <ktahsin280@gmail.com>
2025-06-22 20:24:11 +0600

    updated

--- Backend/src/main/java/com/tahsin/backend/Configuration/WebConfig.java
+++ Backend/src/main/java/com/tahsin/backend/Configuration/WebConfig.java
@@ -1,5 +1,6 @@
 package com.tahsin.backend.Configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,11 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig {
+    @Value("${frontend.url}")
+    private String frontendUrl;
     @Bean public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             public void addCorsMappings(CorsRegistry registry){
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins(frontendUrl)
                         .allowedMethods("GET","POST","PUT","DELETE","OPTIONS","PATCH")
                         .allowedHeaders("*")
                         .allowCredentials(true);
