7cc83564f7760f8bde38316a598be5a2dfab4e83
Tahsin <ktahsin280@gmail.com>
2025-07-19 05:50:40 +0600

    review rest added

---
+++ Backend/src/main/java/com/tahsin/backend/Controller/ReviewController.java
@@ -0,0 +1,37 @@
+package com.tahsin.backend.Controller;
+import com.tahsin.backend.dto.ReviewRequestDTO;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tahsin.backend.Model.Review;
+import com.tahsin.backend.Service.AppointmentService;
+import com.tahsin.backend.Service.ReviewService;
+
+@RestController
+@RequestMapping("/api/reviews")
+public class ReviewController {
+    // Add methods to handle review requests
+    // For example, a method to create a new review
+    @Autowired 
+    private ReviewService reviewService;
+    @Autowired
+    private AppointmentService appointmentService;
+    
+    @PostMapping
+    public ResponseEntity<Review> createReview(@RequestBody ReviewRequestDTO reviewRequest) {
+        // Logic to create a review based on the request
+        Review newReview = new Review();
+        newReview.setAppointment(appointmentService.getById(reviewRequest.getAppointmentId()));
+        newReview.setRating(reviewRequest.getRating());
+        newReview.setComment(reviewRequest.getComment());
+        
+        Review savedReview = reviewService.save(newReview);
+        return ResponseEntity.ok(savedReview);
+    }
+
+}
